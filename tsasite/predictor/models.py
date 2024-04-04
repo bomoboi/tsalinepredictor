71009ace01994ace2f87b7f46ec08a31a90d4bbc
a.js@posteo.net <a.js@posteo.net>
2024-04-04 04:47:36 -0400

    redid modeling and added to admin page

--- tsasite/predictor/models.py
+++ tsasite/predictor/models.py
@@ -5,13 +5,15 @@ class Airport(models.Model):
     icao_code = models.CharField("the ICAO code of the airport", max_length=4)
     ap_name = models.CharField("the name of the airport", max_length=64)
 
-class Checkpoint():
-    checkpoint_name = models.CharField("the name of the checkpoint", max_length=64)
-    iata_code = models.ForeignKey(Airport, on_delete=models.CASCADE, verbose_name="the airport the checkpoint is in")
+    def __str__(self):
+        return f"({self.iata_code}/{self.icao_code}: {self.ap_name})"
 
 class Record(models.Model):
     date = models.DateField("the date of the record")
     hour = models.PositiveSmallIntegerField("the hour of the record")
     pax = models.PositiveSmallIntegerField("the number of passengers given by the record")
     ap_code = models.ForeignKey(Airport, on_delete=models.CASCADE, verbose_name="the airport the record belongs to")
-    checkpoint = models.ForeignKey(Checkpoint, on_delete=models.RESTRICT, verbose_name="the checkpoint of the record")
+    checkpoint = models.CharField("the name of the checkpoint in the record", max_length=64)
+
+    def __str__(self):
+        return f"({self.date.__str__()}, {self.hour:02d}:00, {self.ap_code.__str__()}, {self.checkpoint}, {self.pax:,})"
