d092425f8277f8a84b8782374d74a90703fade4e
a.js@posteo.net <a.js@posteo.net>
2024-04-15 21:20:03 -0400

    implemented histogram functionality
    - removed unnecessary imports
    - added comments
    - restructured output for easier referencing by template

--- tsasite/predictor/utils.py
+++ tsasite/predictor/utils.py
@@ -1,5 +1,5 @@
 from .models import Record, Airport
-from django.db.models import Avg, Max, Q
+from django.db.models import Avg
 from datetime import datetime, timedelta
 
 """
@@ -18,16 +18,18 @@ def get_records(date: datetime,
                 checkpoint: str,
                 var_hour: int):
 
-    start_time = date - timedelta(hours=var_hour)
-    end_time = date + timedelta(hours=var_hour)
-
+    # gets the initial QuerySet
     qs = Record.objects.filter(
         ap=airport
     ).filter(
         checkpoint=checkpoint)
 
-    avg_set = {}
+    avg_dict = {}
     for h in range(-var_hour, var_hour+1):
+        # gets the data for each hour across all years
+        # in the database
+        # HACK maybe change this to pre-covid only?
+        # or an option for admin
         filter_date = date + timedelta(hours=h)
         h_qs = qs.filter(
             date__hour=filter_date.hour
@@ -36,5 +38,14 @@ def get_records(date: datetime,
         ).filter(
             date__year__lt=filter_date.year
         )
-        avg_set[str(h)] = h_qs.aggregate(Avg("pax", default=0))["pax__avg"]
-    return avg_set
+        # creates a dictionary where the keys are in the format 10 am, 11am ,etc
+        # and the values are the average time for that hour
+        avg_dict[
+            filter_date.strftime("%I %p").lower()] = h_qs.aggregate(
+            Avg("pax", default=0))["pax__avg"]
+
+        # make sure the central value is highlighted in a different color
+    avg_dict[date.strftime("%I %p").lower()] = {"y": avg_dict[date.strftime("%I %p").lower()],
+                                                    "color": "pink"}
+    print(avg_dict)
+    return avg_dict
