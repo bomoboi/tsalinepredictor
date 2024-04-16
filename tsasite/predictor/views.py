ae3c42072a5644b2e35573f9f1b3a236f504f2d0
a.js@posteo.net <a.js@posteo.net>
2024-04-15 21:29:46 -0400

    added results page functionality
    this should really be under the results-page branch, whoops

--- tsasite/predictor/views.py
+++ tsasite/predictor/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 from datetime import datetime
 
 from .utils import get_records
@@ -18,5 +19,10 @@ def user(request):
 def results(request):
     ap = Airport.objects.get(pk="LAX")
     dt = datetime(year=2024, month=4, day=15, hour=11, minute=0, second=0)
-    records = get_records(dt, ap, "CHK1", 2)
-    return HttpResponse(f"{records}")
+    records = get_records(dt, ap, "CHK1", 1)
+
+    template = loader.get_template("predictor/results.html")
+    context = {"result": records[dt.strftime("%I %p").lower()]['y'],
+               "times": list(records.keys()),
+               "avgs": list(records.values())}
+    return render(request, "predictor/results.html", context)
